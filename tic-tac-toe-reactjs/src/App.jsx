c093f9f9819e876a01f22429e79bd77c735a5558
JoaoVictor-C <fffjv2006@gmail.com>
2023-12-10 09:48:42 -0300

    Algumas mudanças

---
+++ tic-tac-toe-reactjs/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Home from './pages/Home'
+import CreateGame from './pages/CreateGame'
+import JoinGame from './pages/JoinGame' 
+import OfflineGame from './pages/OfflineGame'
+
+import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/tic-tac-toe-ReactJS" element={<Home />} />
+            <Route path="/tic-tac-toe-ReactJS/create" element={<CreateGame />} />
+            <Route path="/tic-tac-toe-ReactJS/join" element={<JoinGame />} />
+            <Route path="/tic-tac-toe-ReactJS/offline" element={<OfflineGame />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ tic-tac-toe-reactjs/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+       <div className="home">
+            <h1>Jogo da velha!</h1>
+            <div className="links">
+                <a href="/tic-tac-toe-ReactJS/create">Criar</a>
+                <a href="/tic-tac-toe-ReactJS/join">Entrar</a>
+                <a href="/tic-tac-toe-ReactJS/offline">Offline</a>
+            </div>
+       </div>
+    )
+}
+
+export default Home;
